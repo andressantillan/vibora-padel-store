e70dffc5b17138c7bed8cb883e68c17a6a170450
Andres Santillan <afs9719@gmail.com>
2026-04-13 17:46:16 -0300

    Logo de aplicacion y config de base

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,11 +13,18 @@
         <!-- Bootstrap -->
         <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     </head>
+    <style>
+        .logo {
+            max-width: 300px;
+            height: auto;
+        }
+    </style>
     <body>
 
         @yield('content')
-        <div class="container py-5">
+        <div class="container py-5 text-center">
             <h2>Vibora Padel Store</h2>
+            <img src="{{ asset('images/logo-web.png') }}" alt="Vibora Padel Store Logo" class="img-fluid mb-4 logo">
             <p>Sitio de venta de productos de padel</p>
             <p>Integrantes: Santillán Andrés</p>
         </div>
