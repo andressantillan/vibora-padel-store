a1d83291c93a2fbd5fc7f4b926cdd5bd9dc9afb8
Andres Santillan <afs9719@gmail.com>
2026-06-07 19:02:11 -0300

    fix(deploy): invalidar caché de paquetes pegada en /tmp de la lambda

--- api/index.php
+++ api/index.php
@@ -3,8 +3,7 @@
 $host = $_SERVER['HTTP_HOST'] ?? 'localhost';
 
 // Vercel usa X-Forwarded-Proto, no $_SERVER['HTTPS']
-$proto = $_SERVER['HTTP_X_FORWARDED_PROTO'] 
-      ?? (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ? 'https' : 'http');
+$proto = $_SERVER['HTTP_X_FORWARDED_PROTO'] ?? (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ? 'https' : 'http');
 
 $url = $proto . '://' . $host;
 
@@ -13,17 +12,5 @@ putenv('ASSET_URL=' . $url);
 $_ENV['APP_URL'] = $url;
 $_ENV['ASSET_URL'] = $url;
 $host = $_SERVER['HTTP_HOST'] ?? 'localhost';
-
-// Vercel usa X-Forwarded-Proto, no $_SERVER['HTTPS']
-$proto = $_SERVER['HTTP_X_FORWARDED_PROTO'] 
-      ?? (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' ? 'https' : 'http');
-
-$url = $proto . '://' . $host;
-
-putenv('APP_URL=' . $url);
-putenv('ASSET_URL=' . $url);
-$_ENV['APP_URL'] = $url;
-$_ENV['ASSET_URL'] = $url;
-
 
 require (__DIR__.'/../public/index.php');
